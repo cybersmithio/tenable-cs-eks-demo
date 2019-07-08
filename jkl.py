588550561ace1ff33e3d9c2e59180976221c9a43
jamessmith <jamessmith>
2019-07-08 16:29:18 -0400

    Added pvcs.yaml in case we want to demonstrate persistent volume claims
    Updated Kubernetes library to be able to create and delete a namespace

--- jkl.py
+++ jkl.py
@@ -2,7 +2,9 @@
 #[J]ames [K]ubernetes [L]ibrary
 
 from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 import sys
+from pprint import pprint
 
 def checkRunningPods(DEBUG):
     # Configs can be set in Configuration class directly or using helper utility
@@ -23,3 +25,30 @@ def checkRunningPods(DEBUG):
             print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
     return(countpods)
+
+def createNamespace(DEBUG,namespace):
+    # Configs can be set in Configuration class directly or using helper utility
+    config.load_kube_config()
+
+    kube = client.CoreV1Api()
+    body=client.V1Namespace(metadata=client.V1ObjectMeta(name=namespace))
+
+
+    try:
+        api_response = kube.create_namespace(body)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling CoreV1Api->create_namespace: %s\n" % e)
+
+
+def deleteNamespace(DEBUG,namespace):
+    # Configs can be set in Configuration class directly or using helper utility
+    config.load_kube_config()
+
+    kube = client.CoreV1Api()
+
+    try:
+        api_response = kube.delete_namespace(namespace)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling CoreV1Api->delete_namespace: %s\n" % e)
